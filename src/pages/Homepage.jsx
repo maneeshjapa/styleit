4c6bae1f6d228d856f309f7e46f6c0121e8cc341
maneeshjapa <jmaneeshh@gmail.com>
2020-04-01 22:17:59 +0530

    Created menu directory and item components. Finished styling with sass

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './../styles/Homepage.scss'
+import MenuDirectory from '../components/directory/Menu-Directory.component'
+
+const Homepage = () => (
+  <div className="homepage">
+    <MenuDirectory />
+  </div>
+);
+
+export default Homepage;
