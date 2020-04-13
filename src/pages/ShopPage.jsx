48f1a329cce5007cfd86cef7a5807fb5c08f9807
maneeshjapa <jmaneeshh@gmail.com>
2020-04-13 12:54:37 +0530

    Added collection component to dynamically render collection based on collectionId

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CollectionOverview from "../components/CollectionOverview.component";
+import {Route} from 'react-router-dom';
+import CollectionPage from '../components/Collection.component';
 
-const ShopPage = () => (
-  <div>
-    <CollectionOverview />
+const ShopPage = ({match}) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionOverview} /> 
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
--- src/redux/shop/ShopSelector.js
+++ src/redux/shop/ShopSelector.js
@@ -2,7 +2,22 @@ import { createSelector } from "reselect";
 
 const selectShopData = (state) => state.SHOP_DATA;
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 export const selectCollections = createSelector(
   [selectShopData],
   (SHOP_DATA) => SHOP_DATA.collections
 );
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
